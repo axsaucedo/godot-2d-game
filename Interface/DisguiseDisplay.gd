213819acdefe2fb642e5e2fdbae60e914c4e6ba7
Alejandro Saucedo <axsauze@gmail.com>
2020-05-08 10:32:18 +0100

    Added functionality for gui to display boxes

---
+++ Interface/DisguiseDisplay.gd
@@ -0,0 +1,9 @@
+extends ItemList
+
+const BOX_SPRITE = "res://assets/GFX/PNG/Tiles/tile_130.png"
+
+func update_disguises(number):
+	print(number)
+	clear()
+	for disguises in range(number):
+		add_icon_item(load(BOX_SPRITE), false)
--- Player.gd
+++ Player.gd
@@ -3,18 +3,31 @@ extends "res://Characters/TemplateCharacter.gd"
 var motion = Vector2()
 
 const PLAYER_SPRITE = "res://assets/GFX/PNG/Hitman 1/hitman1_stand.png"
+const PLAYER_LIGHT = "res://assets/GFX/PNG/Hitman 1/hitman1_stand.png"
 const BOX_SPRITE = "res://assets/GFX/PNG/Tiles/tile_130.png"
+const BOX_LIGHT = "res://assets/GFX/PNG/Tiles/tile_130.png"
 const PLAYER_OCCLUDER = "res://Characters/HumanOccluder.tres"
 const BOX_OCCLUDER = "res://Characters/BoxOccluder.tres"
 
+var velocity_multiplier = 1
 var disguised = false
+
+export var disguise_slowdown = 0.25
+export var disguise_duration = 5
+export var number_of_disguises = 3
 
 func _ready():
 	AudioServer.set_bus_volume_db(0, -12)
+	$Timer.wait_time = disguise_duration
+	update_disguise_display()
+	reveal()
 
 func _physics_process(delta):
 	update_movement()
-	move_and_slide(motion)
+	move_and_slide(motion * velocity_multiplier)
+	if disguised:
+		$DisguiseLabel.text = str($Timer.time_left).pad_decimals(2)
+		$DisguiseLabel.rect_rotation = -rotation_degrees
 
 func update_movement():
 	look_at(get_global_mouse_position())
@@ -41,17 +54,29 @@ func _input(event):
 func toggle_disguise():
 	if disguised:
 		reveal()
-	else:
+	elif number_of_disguises > 0:
 		disguise()
 
 func reveal():
 	$Sprite.texture = load(PLAYER_SPRITE)
+	$Light2D.texture = load(PLAYER_LIGHT)
 	$LightOccluder2D.occluder = load(PLAYER_OCCLUDER)
+	$DisguiseLabel.hide()
+	velocity_multiplier = 1
 	disguised = false
 	collision_layer = 1
 
 func disguise():
 	$Sprite.texture = load(BOX_SPRITE)
+	$Light2D.texture = load(BOX_LIGHT)
 	$LightOccluder2D.occluder = load(BOX_OCCLUDER)
+	$DisguiseLabel.show()
+	velocity_multiplier = disguise_slowdown
+	number_of_disguises -= 1
+	update_disguise_display()
 	disguised = true
 	collision_layer = 16
+	$Timer.start()
+	
+func update_disguise_display():
+	get_tree().call_group("DisguiseDisplay", "update_disguises", number_of_disguises)
